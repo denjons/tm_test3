8f11bfaa2505fd1dd653fc79ac05ceeb36f18246
jonssonde <dennis.jonsson@cgi.com>
2017-09-11 13:43:48 +0200

    first commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM maven:3.5
+
+
+# Install necessary packages
+COPY . /src
+
+RUN cd /src && mvn clean package && cd ./target && mv test3-wildfly.jar /app
+
+EXPOSE 8080
+
+CMD cd /app && java -jar test3-wildfly.jar
---
+++ src/test/java/org/test/rest/it/EndpointTest.java
@@ -0,0 +1,16 @@
+package org.test.rest.it;
+
+import static org.junit.Assert.*;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class EndpointTest {
+
+    @Test
+    public void mockTest(){
+        assertTrue(true);
+    }
+
+}
